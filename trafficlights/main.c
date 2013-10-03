92a0b05c44fd7db88d9c8dc3594c85330d12af26
Kaj Nelissen <kaj@kj91.nl>
2013-10-03 12:58:05 +0200

    Creatie project

---
+++ trafficlights/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Kaj
+ *
+ * Created on 3 oktober 2013, 12:56
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
